8126232d54d80517ded2e5ceeab1df5e796dbd56
Ashvika-Anumula <107846312+Ashvika-Anumula@users.noreply.github.com>
2023-07-13 20:14:58 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+function start()
+{
+  window.location="fruit.html";
+}
+
+function start1()
+{
+  window.location="kitchen.html";
+}
+
+
+function start2()
+{
+  window.location="toys.html";
+}
+
+function start3()
+{
+  window.location="wardrobe.html";
+}
+
+function start4()
+{
+  window.location="bedroom.html";
+}
